2c8b350ce509b9d4a8dfff1f70d0247130448b12
Fatih İlhan <fatilhan1507@gmail.com>
2025-02-11 19:20:37 +0300

    6. ders bitti

---
+++ RealEstate_Dapper_Api/Controllers/CategoriesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RealEstate_Dapper_Api.Dtos.CategoryDtos;
+using RealEstate_Dapper_Api.Repositories.CategoryRepository;
+
+namespace RealEstate_Dapper_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        public CategoriesController(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CategoryList()
+        {
+            return Ok(await _categoryRepository.GetAllCategoryAsync());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryDto createCategoryDto)
+        {
+            _categoryRepository.CreateCategoryAsync(createCategoryDto);
+            return Ok("Kategori Başarılı Bir Şekilde Eklendi");
+        }
+    }
+}
